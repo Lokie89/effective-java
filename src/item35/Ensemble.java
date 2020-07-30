be584c175956a5c45bd2fa29f3355ddf2f55df1d
Lokie89 <traeuman@gmail.com>
2020-07-30 10:28:40 +0900

    item-35 ordinal 메서드 대신 인스턴스 필드를 사용하라

---
+++ src/item35/Ensemble.java
@@ -0,0 +1,31 @@
+package item35;
+
+/**
+ * @author lokie on 2020-07-30
+ */
+public enum Ensemble {
+    SOLO(1),
+    DUET(2),
+    TRIO(3),
+    QUARTET(4),
+    QUINTET(5),
+    SEXTET(6),
+    SEPTET(7),
+    OCTET(8),
+    DOUBLE_QUARTET(8),
+    NONET(9),
+    DECTET(10),
+    TRIPLE_QUARTET(12)
+    ;
+
+    private final int numberOfMusicians;
+
+    Ensemble(int numberOfMusicians) {
+        this.numberOfMusicians = numberOfMusicians;
+    }
+
+    public int numberOfMusicians() {
+//        return ordinal() + 1;
+        return numberOfMusicians;
+    }
+}
