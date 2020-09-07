2bd4ea948de279de81da7836654ccda0c186da5c
Lokie89 <traeuman@gmail.com>
2020-09-07 11:19:43 +0900

    item-84 프로그램의 동작을 스레드 스케줄러에 기대지 말라

---
+++ src/item84/SlowCountDown.java
@@ -0,0 +1,28 @@
+package item84;
+
+public class SlowCountDown {
+    private int count;
+
+    public SlowCountDown(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException(count + "< 0");
+        }
+        this.count = count;
+    }
+
+    public void await() {
+        while (true) {
+            synchronized (this) {
+                if (count == 0) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public synchronized void countDown() {
+        if (count != 0) {
+            count--;
+        }
+    }
+}
