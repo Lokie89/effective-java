d4ce893f5fa84570e93096828c61f848719b2893
Lokie89 <traeuman@gmail.com>
2020-09-08 13:35:42 +0900

    item-87 커스텀 직렬화 형태를 고려해보라

---
+++ src/item87/StringList.java
@@ -0,0 +1,14 @@
+package item87;
+
+import java.io.Serializable;
+
+public class StringList implements Serializable {
+    private int size = 0;
+    private Entry head = null;
+
+    private static class Entry implements Serializable {
+        String data;
+        Entry next;
+        Entry previous;
+    }
+}
